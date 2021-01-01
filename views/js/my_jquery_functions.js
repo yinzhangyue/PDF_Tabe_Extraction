742301b3a23abe2bee758e6c066077c64de0ebec
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2021-01-02 06:17:51 +0800

    pdf页数跳转added

--- views/js/my_jquery_functions.js
+++ views/js/my_jquery_functions.js
@@ -25,11 +25,31 @@ function get_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_nu
         }
     };
     window.onload = function() {
-        console.log(url);
-        console.log("#" + divID);
+        // console.log(url);
+        // console.log("#" + divID);
         console.log(options);
         PDFObject.embed(url, "#" + divID, options);
     }
+}
+
+function jump_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_num = 1) {
+    var options = {
+        //If browser doesn't support inline PDFs
+        // fallbackLink: "<p>This is a <a href='[url]'>fallback link</a></p>";
+        height: "800px",
+        id: divID,
+        page: page_num.toString(),
+        pdfOpenParams: {
+            view: 'FitV',
+            // page: page_num,
+            // comment: "452fde0e-fd22-457c-84aa2cf5bed5a349"
+        }
+    };
+    // console.log(url);
+    // console.log("#" + divID);
+    console.log(options);
+    PDFObject.embed(url, "#" + divID, options);
+
 }
 
 function version() {
