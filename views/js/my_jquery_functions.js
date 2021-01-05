0608f81644794170853e2b0bb535f75556ff0242
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2021-01-05 14:43:52 +0800

    数据定义&函数定义

--- views/js/my_jquery_functions.js
+++ views/js/my_jquery_functions.js
@@ -11,6 +11,25 @@ let test_url = "http://localhost:8080/views/test.pdf";
 let test_divID = "pdf"
 let test_pdfID = "user00+filename+create_time";
 
+function upload(pdfID) {
+    //生成pdf文件编号
+    //传输到服务端
+    //await get_data()后端数据的返回
+    //最后return ok
+}
+
+async function get_date(pdfID) {
+    //数据格式: pdfID ——> [[(pagenum, tableID)], [(pagenunm, tableID),(pagenum, tableID)]...] 即每页上表格数为&  [1,2,...]
+}
+
+function generate_List() {
+    //在html动态地插入<>
+}
+
+function download_exl() {
+
+}
+
 function get_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_num = 1) {
     var options = {
         //If browser doesn't support inline PDFs
@@ -29,6 +48,8 @@ function get_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_nu
         // console.log("#" + divID);
         console.log(options);
         PDFObject.embed(url, "#" + divID, options);
+        //TODO: 加载页面时动态地生成sidebar list
+        generate_List();
     }
 }
 
