aa93f03bd772585762dded5ecc434e87fab42543
梅佳奕 <10165300206@stu.ecnu.edu.cn>
2021-01-10 20:09:50 +0800

    上传卡bug

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var PORT = 3000;
 
 ////////////////////////////////////////////////Dependency////////////////////////////////////////////////
 var path = require('path');
+const request = require('request');
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var express = require('express');
+var multiparty = require('multiparty');
 var app = express();
 
 
@@ -29,6 +31,7 @@ app.use(function(req, res, next) {
     next();
 });
 app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -42,9 +45,13 @@ var Request = {
     content: ''
 }
 
+
+
+
 //////////////////////////////////////////////////rout////////////////////////////////////////////////////
 //服务器主页输出
 app.get('/', function(req, res) {
+    console.log(req.body);
     // Request.title = req.query.title;
     // Request.kw = req.query.kw;
     // Request.content = req.query.content;
@@ -62,9 +69,41 @@ app.get('/', function(req, res) {
     res.render('index.html', { title: 'HTML' });
     // res.redirect('/news');
     // res.send(req.body);
-
 });
 
+
+function upload(file) {
+    //生成pdf文件编号
+    //传输到服务端
+    //await get_data()后端数据的返回
+    //最后return chunk
+    request.post("http://113.31.124.254:5000/pdf/upload", {
+        formData: {
+            // "name": "testPDF-cvpr2015",
+            // "file": fs.createReadStream(pdfPATH = "D:/cvpr2015.pdf")
+            "file": file
+        }
+    }, function(err, res, body) {
+        if (body) {
+            console.log(body);
+        }
+        if (err) {
+            console.log("ERROR! " + err);
+        }
+    });
+}
+app.post('/upload', function(req, res) {
+    // var pdfFile = req.data;
+
+    let form = new multiparty.Form();
+    form.parse(req, function(err, fields, file) {
+        console.log(file);
+        upload(file);
+    })
+    console.log("收到一个上传请求");
+    res.send(true);
+
+});
 
 app.get('/pdf', function(req, res) {
     res.render('browsePDF.html', { title: 'HTML' });
--- views/js/my_jquery_functions.js
+++ views/js/my_jquery_functions.js
@@ -1,3 +1,53 @@
+//////////////////////////////////////////////////////////前后端数据交互///////////////////////////////////////////////////
+// const request = require('request');
+// var fs = require('fs');
+
+
+async function get_data(pdfID) {
+    //数据格式: pdfID ——> [[(pagenum, tableID)], [(pagenunm, tableID),(pagenum, tableID)]...] 即每页上表格数为&  [1,2,...]
+}
+
+function generate_List(Info) {
+    //在html动态地插入<>
+    var PAGE = "<template slot='title' id='pageX'><i class='el-icon-s-data'></i>第x页</template>";
+    var TABLE = "<el-menu-item index='1-1' id='tableX' class='table'>选项x</el-menu-item>";
+    var FRONTTAG = "<el-menu-item-group>";
+    var ENDTAG = "</el-menu-item-group>";
+
+    var Segment = "";
+    for (var i = 0; i < Info.length(); i++) {
+
+        for (var j = 0; j < Info[i].length; j++) {
+
+        }
+    }
+
+
+}
+
+var buildHTML = function(tag, html, attrs) {
+
+    // you can skip html param
+
+    if (typeof(html) != 'string') {
+        attrs = html;
+        html = null;
+    }
+    varh = '<' + tag;
+    for (attr in attrs) {
+        if (attrs[attr] === false) continue;
+        h += ' ' + attr + '="' + attrs[attr] + '"';
+    }
+    return h += html ? ">" + html + "</" + tag + ">" : "/>";
+};
+
+function download_exl() {
+
+}
+
+
+//////////////////////////////////////////////////////////渲染pdf页面///////////////////////////////////////////////////
+
 function check_support() {
     if (PDFObject.supportsPDFs) {
         console.log("Yay, this browser supports inline PDFs.");
@@ -10,25 +60,6 @@ function check_support() {
 let test_url = "http://localhost:8080/views/test.pdf";
 let test_divID = "pdf"
 let test_pdfID = "user00+filename+create_time";
-
-function upload(pdfID) {
-    //生成pdf文件编号
-    //传输到服务端
-    //await get_data()后端数据的返回
-    //最后return ok
-}
-
-async function get_date(pdfID) {
-    //数据格式: pdfID ——> [[(pagenum, tableID)], [(pagenunm, tableID),(pagenum, tableID)]...] 即每页上表格数为&  [1,2,...]
-}
-
-function generate_List() {
-    //在html动态地插入<>
-}
-
-function download_exl() {
-
-}
 
 function get_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_num = 1) {
     var options = {
@@ -49,7 +80,8 @@ function get_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_nu
         console.log(options);
         PDFObject.embed(url, "#" + divID, options);
         //TODO: 加载页面时动态地生成sidebar list
-        generate_List();
+        // generate_List();
+        // $("#1-1").text("Hellooo");
     }
 }
 
@@ -60,11 +92,11 @@ function jump_pdf(url = test_url, divID = test_divID, pdfID = test_pdfID, page_n
         height: "800px",
         id: divID,
         page: page_num.toString(),
-        pdfOpenParams: {
-            view: 'FitV',
-            // page: page_num,
-            // comment: "452fde0e-fd22-457c-84aa2cf5bed5a349"
-        }
+        // pdfOpenParams: {
+        //     view: 'FitV',
+        // page: page_num,
+        // comment: "452fde0e-fd22-457c-84aa2cf5bed5a349"
+        // }
     };
     // console.log(url);
     // console.log("#" + divID);
